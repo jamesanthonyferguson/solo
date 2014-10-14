4b7fa59d841b2548dafd677b292f2630d1e09926
jamesanthonyferguson <jamesanthonyferguson@gmail.com>
2014-10-13 22:58:00 -0700

    create styling

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -14,9 +14,20 @@ angular.module('201409SoloApp')
         var temp = {q: $scope.question, a: {1: $scope.a1, 2: $scope.a2, 3: $scope.a3, 4: $scope.a4}, correct: $scope.correct}
         $scope.list.push(temp);
         $http.post('/api/things', temp)
-          .success(console.log('posted'));
+          .success(function(){
+            console.log('posted');
+            $scope.reset();
+          });
       // $scope.submit(temp);
       }
+   }
+   $scope.reset = function(){
+    $scope.question = undefined;
+    $scope.a1 = undefined;
+    $scope.a2 = undefined;
+    $scope.a3 = undefined;
+    $scope.a4 = undefined;
+    $scope.correct = undefined;
    }
     // $scope.submit = function(data){
     //   $http.post('/api/things', data)
