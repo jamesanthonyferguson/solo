a610333ab960aab3120107780082712d925f9316
jamesanthonyferguson <jamesanthonyferguson@gmail.com>
2014-10-13 22:15:26 -0700

    basic form validation

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -9,13 +9,15 @@ angular.module('201409SoloApp')
     $scope.a4;
     $scope.correct;
     $scope.list = []
-    $scope.add = function(){
-      var temp = {q: $scope.question, a: {1: $scope.a1, 2: $scope.a2, 3: $scope.a3, 4: $scope.a4}, correct: $scope.correct}
-      $scope.list.push(temp);
-      $http.post('/api/things', temp)
-        .success(console.log('posted'));
+    $scope.add = function(bool){
+        if (bool) {
+        var temp = {q: $scope.question, a: {1: $scope.a1, 2: $scope.a2, 3: $scope.a3, 4: $scope.a4}, correct: $scope.correct}
+        $scope.list.push(temp);
+        $http.post('/api/things', temp)
+          .success(console.log('posted'));
       // $scope.submit(temp);
-    }
+      }
+   }
     // $scope.submit = function(data){
     //   $http.post('/api/things', data)
     //   .success(console.log('posted'))
